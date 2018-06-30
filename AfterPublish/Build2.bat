2e549fde4f5b042b1426bfe5959ce6fc286b4b5e
Ashley Mallia <ashley.mallia@akqa.com>
2018-06-30 20:14:55 +1000

    Publishing to single directory working perfectly

--- AfterPublish/Build2.bat
+++ AfterPublish/Build2.bat
@@ -6,33 +6,21 @@ SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBu
 
 REM <!-- <Target Name="GatherAllFilesToPublish" DependsOnTargets="PipelineTransformPhase;CopyAllFilesToSingleFolderForPackage;" /> -->
 
-REM %MSBUILD%	AfterPublishTarget.sln /t:Build,Package
-
-
-REM _PackageTempDir
-REM t Package vs WebPublish
-
-REM "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" AfterPublishTarget.sln ^
-REM /p:DeployOnBuild=true ^
-REM /p:DeployTarget=Package ^
-REM /p:_PackageTempDir=pppp ^
-REM /p:PackageAsSingleFile=False ^
-REM /p:AutoParameterizationWebConfigConnectionStrings=false
-
-
-REM IT APPEARS THAT THAT SPECIFYING _PackageTempDir to be the same location as PublishUrl actually
-REM prevents one more set of copies :)
+REM THE HOLY GRAIL OF PUBLISHING A WEB APPLICATION TO A FOLDER
+REM THIS COMBINATION OF ARGUMENTS:
+REM 1. PERFORMS AN INCREMENTAL BUILD
+REM 2. PUBLISES ASP WEB APPLICATION FILES TO %CD%\BUILT
+REM 3. ONLY WRITES TO BUILT FOLDER WHEN CHANGES OCCUR
 
 %MSBUILD%	AfterPublishTarget.sln /T:BUILD /M /DETAILEDSUMMARY /VERBOSITY:NORMAL ^
 			/p:Configuration=Release ^
             /p:DeployOnBuild=True ^
-            /p:DeployDefaultTarget=WebPublish ^
-            /p:WebPublishMethod=FileSystem ^
+            /p:DeployTarget=PipelinePreDeployCopyAllFilesToOneFolder ^
             /p:DeleteExistingFiles=False ^
-			/p:publishUrl="%cd%\_\Publish" ^
-			/p:_PackageTempDir="%cd%\_\Publish" ^
-			/p:_FindDependencies=False ^
-			/p:MSDeployUseChecksum=True
+			/p:_PackageTempDir="%cd%\built" ^
+			/p:_FindDependencies=False > build2.txt
 			
-REM http://www.zvolkov.com/clog/2010/05/18/how-to-packagepublish-web-site-project-using-vs2010-and-msbuild/
-			
+REM  <!-- In Project disable the Transform and Parameterization features of WPP -->
+REM  <Target Name="TransformWebConfig" />
+REM  <Target Name="PipelineMsdeploySpecificTransformPhase" />
+
